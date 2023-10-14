64b613fa972ebebb1f03f0ec33571fb6410c2f70
AInan05 <147404730+AInan05@users.noreply.github.com>
2023-10-15 00:08:06 +0100

    Update BareBonesInterpreter.java
    Completed the base interpreter. Now it can deal with incr, decr and clear.

--- BareBonesInterpreter.java
+++ BareBonesInterpreter.java
@@ -1,23 +1,75 @@
-/**The Plan.
- * Make a Scanner object to read the contents of the text file containing the "Bare Bones" code.
- * Separate each line into keywords and put them in an array.
- * Create methods for each keyword.
- * Execute methods using the parameters provided in each line of code.
+/** Space Cadets Challenge 2
+ *  Basic interpreter for "BareBones Language".
  */
 
 import java.util.Scanner;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class BareBonesInterpreter {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File bareBonesText = new File("C:\\Users\\user\\Desktop\\test.txt");
-        Scanner codeReader = new Scanner(bareBonesText);
-        while (codeReader.hasNextLine()) {
-            String line = codeReader.nextLine();
-            System.out.println(line);
+    Dictionary<String, Integer> variables = new Hashtable<>();
+    File bareBonesFile;
+
+    public void interpret(String filePath) {
+        try {
+            bareBonesFile = new File(filePath);
+            Scanner codeReader = new Scanner(bareBonesFile);
+            while (codeReader.hasNextLine()) {
+                String line = codeReader.nextLine();
+                String[] stuff = line.split(" ");
+                execute(stuff[0], stuff[1]);
+                System.out.println(variables);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
         }
+    }
+
+    public void execute(String keyword, String name) {
+        switch(keyword) {
+            // deal with ; at the end later
+            case "clear":
+                clear(name);
+                break;
+            case "incr":
+                incr(name);
+                break;
+            case "decr":
+                decr(name);
+                break;
+        }
+    }
+
+    public void clear(String name) {
+        variables.put(name, 0);
+    }
+
+    public void incr(String name) {
+        if (variables.get(name) != null) {
+            variables.put(name, (variables.get(name) + 1));
+        }// else {
+         // error message
+        //}
+    }
+
+    public void decr(String name) {
+        if (variables.get(name) != null) {
+            variables.put(name, (variables.get(name) - 1));
+        }// else {
+        // error message
+        //}
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Please enter code path: ");
+        Scanner inputListener = new Scanner(System.in);
+        String codePath = inputListener.nextLine();
+        BareBonesInterpreter brb = new BareBonesInterpreter();
+        brb.interpret(codePath);
     }
 }
 
+// Test file: C:\\Users\\user\\Desktop\\test.txt
