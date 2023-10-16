ece3e8c2acecb1b56e229b5d441ed2f61ccd19ba
AInan05 <147404730+AInan05@users.noreply.github.com>
2023-10-16 03:12:13 +0100

    Update BareBonesInterpreter.java
    Fixed the issue with while loops when condition was false from the start of the while loop. Half way done with error handling.

--- BareBonesInterpreter.java
+++ BareBonesInterpreter.java
@@ -1,11 +1,24 @@
 /** Space Cadets Challenge 2
  *  Basic interpreter for "BareBones Language".
  *  regular lines -> keyword(\s(condition\sdo)|(varName))?;
+ *  List of possible error messages:
+ *  1. Value cannot be negative -
+ *  2. var not found -
+ *  3. File not found -
+ *  4. Not a valid file type -
+ *  5. Unexpected indentation
+ *  6. Syntax Error -
+ *  7. Unexpected end
  */
 
-import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class BareBonesInterpreter {
 
@@ -16,17 +29,23 @@ public class BareBonesInterpreter {
     Integer lineNo = 0;
     Integer endLineNo = 0;
     Stack<Integer> whileStack = new Stack<>();
+    String operations = "incr|decr|clear";
+    String iterations = "while";
+    boolean skip = false;
 
-    public void openFile(String filePath) {
+    public String openFile(String filePath) {
         try {
+            if (!filePath.endsWith(".txt")){
+                return "Invalid file type";
+            }
             bareBonesFile = new File(filePath);
             Scanner codeReader = new Scanner(bareBonesFile);
             while (codeReader.hasNextLine()) {
                 code.add(codeReader.nextLine());
             }
-                // System.out.println(line.matches(this.lineRegex));
+            return "pass";
         } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+            return "File not found";
         }
     }
 
@@ -34,69 +53,102 @@ public class BareBonesInterpreter {
         while (lineNo < code.size()) {
             String s = code.get(lineNo);
             if (indentation >= 3) {
-                if (!s.substring(indentation - 3).startsWith("end;")) {
-                    s = s.substring(indentation);
-                } else {
+                if (s.substring(indentation - 3).startsWith("end;")) {
                     indentation -= 3;
                     endLineNo = lineNo;
-                    lineNo = whileStack.pop();
+                    if (!skip) {
+                        lineNo = whileStack.pop();
+                    }
                     s = code.get(lineNo);
-                    s = s.substring(indentation);
+                    if (skip) {
+                        skip = false;
+                    }
                 }
+                s = s.substring(indentation);
             }
-            String[] stuff = s.split("\\s|;");
-            execute(stuff);
+            if (!skip) {
+                String errorCheck = execute(s);
+            }
             lineNo++;
         }
     }
 
-    public void execute(String[] stuff) {
+    public String execute(String s) {
+        String[] stuff = s.split("\\s|;");
         String keyword = stuff[0];
+        String errorCheck = syntaxErrorCheck(keyword, s);
+        if (errorCheck.equals("pass")) {
+            return errorCheck;
+        }
         switch(keyword) {
-            // deal with ; at the end later
             case "clear":
                 clear(stuff[1]);
                 System.out.println(variables);
                 break;
             case "incr":
-                incr(stuff[1]);
+                errorCheck = incr(stuff[1]);
                 System.out.println(variables);
                 break;
             case "decr":
-                decr(stuff[1]);
+                errorCheck = decr(stuff[1]);
                 System.out.println(variables);
                 break;
             case "while":
                 bbwhile(stuff[1], Integer.valueOf(stuff[3]));
         }
+        return errorCheck;
+    }
+
+    public String syntaxErrorCheck(String keyword, String line) {
+        if (keyword.matches(operations)) {
+            String operationRegex = keyword + "\\s[a-z]+;";
+            if (line.matches(operationRegex)) {
+                return "pass";
+            } else {
+                return "Syntax Error";
+            }
+        } else if (keyword.matches(iterations)) {
+            String iterationRegex = keyword + "\\s[a-z]+\\snot\\d;";
+            if (line.matches(iterationRegex)) {
+                return "pass";
+            } else {
+                return "Syntax Error";
+            }
+        }
+        return "Keyword Error";
     }
 
     public void clear(String name) {
         variables.put(name, 0);
     }
 
-    public void incr(String name) {
+    public String incr(String name) {
         if (variables.get(name) != null) {
             variables.put(name, (variables.get(name) + 1));
-        }// else {
-         // error message
-        //}
+            return "pass";
+        } else {
+            return "Var not found";
+        }
     }
 
-    public void decr(String name) {
+    public String decr(String name) {
         if (variables.get(name) != null) {
+            if (variables.get(name) == 0) {
+                return "Values assigned to variables cannot be negative";
+            }
             variables.put(name, (variables.get(name) - 1));
-        }// else {
-        // error message
-        //}
+            return "pass";
+        } else {
+            return "Var not found";
+        }
     }
 
     public void bbwhile(String name, Integer num) {
+        indentation += 3;
         if (variables.get(name).equals(num)) {
-            lineNo = endLineNo;
+            skip = true;
         } else {
             whileStack.push(lineNo);
-            indentation += 3;
         }
     }
 
