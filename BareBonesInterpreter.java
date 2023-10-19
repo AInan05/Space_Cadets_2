7909e8a62eed99b454d385d2b8fa4bc346d6d44e
AInan05 <147404730+AInan05@users.noreply.github.com>
2023-10-19 23:15:39 +0100

    Update BareBonesInterpreter.java
    Comments and bugfixes.

--- BareBonesInterpreter.java
+++ BareBonesInterpreter.java
@@ -1,7 +1,7 @@
 /** Space Cadets Challenge 2
  *  Basic interpreter for "BareBones Language".
  */
-
+// import necessary libraries
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -24,8 +24,14 @@ public class BareBonesInterpreter {
     boolean skip = false;
     boolean endFound = false;
 
+    /**
+     * Open file method that checks for the correct file type and if the file exists.
+     * If conditions are satisfied open the file.
+     */
+
     public String openFile(String filePath) {
         try {
+            // Check for the correct file type.
             if (!filePath.endsWith(".txt")){
                 return "Invalid file type";
             }
@@ -36,40 +42,52 @@ public class BareBonesInterpreter {
             }
             return "pass";
         } catch (FileNotFoundException e) {
+            // If file does not exist.
             return "File not found";
         }
     }
 
     public String interpret() {
+        // While program has lines to interpret.
         while (lineNo < code.size()) {
+            // Get line.
             String s = code.get(lineNo);
             String errorCheck = "pass";
+            // Check for end statements.
             if (indentation >= 3) {
                 if (s.substring(indentation - 3).startsWith("end;")) {
+                    // Decrease the indentation level.
                     indentation -= 3;
                     endFound = true;
                 }
             }
+            // Remove whitespaces.
             s = s.substring(indentation);
+            // If skip mode is inactivated or an end statement is encountered, execute the code.
             if (!skip || endFound) {
-                errorCheck = execute(s);
+                errorCheck = execute(s, endFound);
             }
+            // Return if an error was encountered.
             if (!errorCheck.equals("pass")) {
                 return errorCheck;
             }
+            // Increment line number.
             lineNo++;
         }
         return "pass";
     }
 
-    public String execute(String s) {
+    public String execute(String s, boolean endFound) {
+        // Divide the line into parts.
         String[] stuff = s.split("\\s|;");
         String keyword = stuff[0];
-        System.out.println(keyword + s);
+        // Run error check.
         String errorCheck = syntaxErrorCheck(keyword, s);
+        // Return if an error was encountered.
         if (!errorCheck.equals("pass")) {
             return errorCheck;
         }
+        // Call methods.
         switch(keyword) {
             case "clear":
                 clear(stuff[1]);
@@ -87,6 +105,9 @@ public class BareBonesInterpreter {
                 bbwhile(stuff[1], Integer.valueOf(stuff[3]));
                 break;
             case "end":
+                if (!endFound){
+                    return "Unexpected end";
+                }
                 end();
                 break;
         }
@@ -95,6 +116,7 @@ public class BareBonesInterpreter {
 
     public String syntaxErrorCheck(String keyword, String line) {
         if (keyword.matches(operations)) {
+            // Check if line matches regex.
             String operationRegex = keyword + "\\s([a-z]|[A-Z])+;";
             if (line.matches(operationRegex)) {
                 return "pass";
@@ -102,6 +124,7 @@ public class BareBonesInterpreter {
                 return "Syntax Error";
             }
         } else if (keyword.matches(iterations)) {
+            // Check if line matches regex.
             String iterationRegex = keyword + "\\s(([a-z]|[A-Z])+\\snot\\s\\d\\sdo);";
             if (line.matches(iterationRegex)) {
                 return "pass";
@@ -115,38 +138,48 @@ public class BareBonesInterpreter {
                 return "Syntax Error";
             }
         }
+        // Unknown keyword.
         return "Keyword Error";
     }
 
     public void clear(String name) {
+        // Assign var 0.
         variables.put(name, 0);
     }
 
     public String incr(String name) {
         if (variables.get(name) != null) {
+            // Increase var.
             variables.put(name, (variables.get(name) + 1));
             return "pass";
         } else {
+            // Var does not exist.
             return "Var not found";
         }
     }
 
     public String decr(String name) {
         if (variables.get(name) != null) {
+            // Check if var is 0.
             if (variables.get(name) == 0) {
                 return "Values assigned to variables cannot be negative";
             }
+            // Decrease var.
             variables.put(name, (variables.get(name) - 1));
             return "pass";
         } else {
+            // Var does not exist.
             return "Var not found";
         }
     }
 
     public void bbwhile(String name, Integer num) {
+        // Increase indentation level.
         indentation += 3;
+        // Push line no into the while stack.
         whileStack.push(lineNo);
         if (variables.get(name) != null){
+            // If condition is false, activate skip mode.
             if (variables.get(name).equals(num)) {
                 skip = true;
             }
@@ -156,9 +189,11 @@ public class BareBonesInterpreter {
     public void end() {
         endFound = false;
         if (skip) {
+            // Set skip to false and remove the last while from while stack.
             skip = false;
             whileStack.pop();
         } else {
+            // Set line number into the last while from the while stack - 1.
             lineNo = whileStack.pop() - 1;
         }
     }
@@ -168,6 +203,7 @@ public class BareBonesInterpreter {
         System.out.println("Please enter code path: ");
         Scanner inputListener = new Scanner(System.in);
         String codePath = inputListener.nextLine();
+        // Initiate object
         BareBonesInterpreter brb = new BareBonesInterpreter();
 
         errorCheck = brb.openFile(codePath);
@@ -175,7 +211,7 @@ public class BareBonesInterpreter {
             errorCheck = brb.interpret();
         }
         if (errorCheck.equals("pass")) {
-            System.out.println("Program was executed successfully.");
+            System.out.println("The program was executed successfully.");
         } else {
             System.out.println("Error at line " + (brb.lineNo + 1) + ": " + errorCheck);
         }
